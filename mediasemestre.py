04980a1ad0288a6bdcb6d64401da1f545de79a14
schuquel <103147624+schuquelsindel@users.noreply.github.com>
2023-03-19 19:48:32 -0300

    Add files via upload

---
+++ mediasemestre.py
@@ -0,0 +1,10 @@
+nota1 = float(input("digite a nota 1:"))
+nota2 = float(input("digite a nota 2:"))
+media = (nota1 + nota2)/2
+print ("A nota final é: %.2f" %(media))
+if (media >= 7):
+      print("aprovado, parabens!")
+else:
+      print("reprovado, tente novamente")
+
+
